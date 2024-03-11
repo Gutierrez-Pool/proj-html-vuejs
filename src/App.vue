466e21a0b9eabf4d7c63155da70f39e4de4bd3a2
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-03-11 11:39:53 +0100

    aggiunto navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+
+  components: {
+    Navbar,
+  }
+}
 </script>
 
 <template>
-  
+  <Navbar></Navbar>
 </template>
 
-<style>
+<style lang="scss">
+@use './styles/variables' as *;
 
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,79 @@
+<script>
+export default {
+    name: 'Navbar',
+
+    data() {
+        return {
+            links: [
+                'Home', 'About', 'Services', 'Blog', 'Contact', 'Portfolio'
+            ],
+
+            activeLink: '0',
+        }
+    },
+}
+</script>
+
+
+<template>
+
+<nav>
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+            <img src="/public/img/white-logo-2.png" alt="logo">
+
+            <div class="d-flex align-items-center gap-5">
+                
+                <ul class="links">
+                    <li
+                        v-for="(link, index) in links"
+                        :class="index == activeLink ? 'active' : ''"
+                    >
+                       {{ link }} 
+                    </li>
+                </ul>
+        
+                <div class="btn btn-primary">
+                    Sign In
+                </div>
+            </div>
+    
+        </div>
+    </div>
+</nav>
+
+</template>
+
+
+<style lang="scss">
+@use '../styles/variables' as *;
+
+nav {
+    padding: 20px 0;
+    background-color: $bg-primary;
+    color: white;
+
+    img {
+        height: 55px;
+    }
+
+    ul {
+        display: flex;
+        
+        margin-bottom: 0;
+        list-style-type: none;
+        gap: 30px;
+
+        li {
+            &.active {
+                color: #F9636B;
+                font-weight: bold;
+            }
+        }
+
+        .btn {
+            // background-color: #1e4286;
+        }
+    }
+}
+</style>
